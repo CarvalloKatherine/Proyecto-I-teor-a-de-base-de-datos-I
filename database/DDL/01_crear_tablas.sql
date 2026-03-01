e830937c5ff8d5208b1fadf5ac1268e39b575f8d
kathsup <katherinencarvallo22@gmail.com>
2026-03-01 11:22:20 -0600

    Cambios en diagrama relacional y en tablas. Cambios en tipo de datos para timestamp, id, bool y double

--- database/DDL/01_crear_tablas.sql
+++ database/DDL/01_crear_tablas.sql
@@ -1,46 +1,45 @@
 CREATE TABLE Usuario (
-    id_usuario              VARCHAR(30)     NOT NULL,
+    id_usuario              INTEGER         IDENTITY NOT NULL,
     clave                   VARCHAR(30),
     primer_nombre           VARCHAR(100),
-    segundo_nombre          VARCHAR(310),
+    segundo_nombre          VARCHAR(100),
     primer_apellido         VARCHAR(100),
     segundo_apellido        VARCHAR(100),
     correo_electronico      VARCHAR(200)    UNIQUE,
     fecha_registro          DATE,
-    salario_mensual_base    DOUBLE,
-    estado                  BIT,
+    salario_mensual_base    NUMERIC(15,2),
+    estado                  BIT             DEFAULT 1,
     creado_por              VARCHAR(200),
-    creado_en               TIMESTAMP,
+    creado_en               TIMESTAMP       DEFAULT CURRENT TIMESTAMP,
     modificado_por          VARCHAR(200),
     modificado_en           TIMESTAMP,
     PRIMARY KEY (id_usuario)
 );
 
 CREATE TABLE Presupuesto (
-    id_presupuesto          VARCHAR(30)     NOT NULL,
-    id_usuario              VARCHAR(30),
+    id_presupuesto          INTEGER         IDENTITY NOT NULL,
+    id_usuario              INTEGER,
     usuario                 VARCHAR(200),
     nombre_descriptivo      VARCHAR(100),
     anio_inicio             INTEGER,
     mes_inicio              INTEGER,
     anio_fin                INTEGER,
     mes_fin                 INTEGER,
-    total_ingresos          DOUBLE,
-    total_gastos            DOUBLE,
-    total_ahorro            DOUBLE,
+    total_ingresos          NUMERIC(15,2),
+    total_gastos            NUMERIC(15,2),
+    total_ahorro            NUMERIC(15,2),
     fecha_hora_creacion     TIMESTAMP,
     estado_presupuesto      VARCHAR(30),
     creado_por              VARCHAR(200),
-    creado_en               TIMESTAMP,
+    creado_en               TIMESTAMP       DEFAULT CURRENT TIMESTAMP,
     modificado_por          VARCHAR(200),
     modificado_en           TIMESTAMP,
     PRIMARY KEY (id_presupuesto),
     FOREIGN KEY (id_usuario) REFERENCES Usuario (id_usuario)
 );
 
-
 CREATE TABLE Categoria (
-    id_categoria            VARCHAR(30)     NOT NULL,
+    id_categoria            INTEGER         IDENTITY NOT NULL,
     nombre_categoria        VARCHAR(30),
     descripcion_detallada   VARCHAR(300),
     tipo_categoria          VARCHAR(30),
@@ -48,40 +47,38 @@ CREATE TABLE Categoria (
     color_hex               VARCHAR(30),
     orden_presentacion      VARCHAR(30),
     creado_por              VARCHAR(200),
-    creado_en               TIMESTAMP,
+    creado_en               TIMESTAMP       DEFAULT CURRENT TIMESTAMP,
     modificado_por          VARCHAR(200),
     modificado_en           TIMESTAMP,
     PRIMARY KEY (id_categoria)
 );
 
-
 CREATE TABLE SubCategoria (
-    id_subcategoria         VARCHAR(30)     NOT NULL,
-    id_categoria            VARCHAR(30),
+    id_subcategoria         INTEGER         IDENTITY NOT NULL,
+    id_categoria            INTEGER,
     categoria_padre         VARCHAR(30),
     nombre_subcategoria     VARCHAR(30),
     descripcion_detallada_sub VARCHAR(300),
-    estado_sub              BIT,
-    por_defecto             BIT,
+    estado_sub              BIT             DEFAULT 1,
+    por_defecto             BIT             DEFAULT 0,
     creado_por              VARCHAR(200),
-    creado_en               TIMESTAMP,
+    creado_en               TIMESTAMP       DEFAULT CURRENT TIMESTAMP,
     modificado_por          VARCHAR(200),
     modificado_en           TIMESTAMP,
     PRIMARY KEY (id_subcategoria),
     FOREIGN KEY (id_categoria) REFERENCES Categoria (id_categoria)
 );
 
-
 CREATE TABLE presupuesto_detalle (
-    id_presupuesto_detalle  VARCHAR(30)     NOT NULL,
-    id_presupuesto          VARCHAR(30),
-    id_subcategoria         VARCHAR(30),
+    id_presupuesto_detalle  INTEGER         IDENTITY NOT NULL,
+    id_presupuesto          INTEGER,
+    id_subcategoria         INTEGER,
     presupuesto_padre       VARCHAR(30),
     subcategoria            VARCHAR(30),
-    monto_mensual_asignado  DOUBLE,
+    monto_mensual_asignado  NUMERIC(15,2),
     observaciones           VARCHAR(100),
     creado_por              VARCHAR(200),
-    creado_en               TIMESTAMP,
+    creado_en               TIMESTAMP       DEFAULT CURRENT TIMESTAMP,
     modificado_por          VARCHAR(200),
     modificado_en           TIMESTAMP,
     PRIMARY KEY (id_presupuesto_detalle),
@@ -91,19 +88,19 @@ CREATE TABLE presupuesto_detalle (
 
 
 CREATE TABLE obligacion_fija (
-    id_obligacion           VARCHAR(30)     NOT NULL,
-    id_subcategoria         VARCHAR(30),
+    id_obligacion           INTEGER         IDENTITY NOT NULL,
+    id_subcategoria         INTEGER,
     usuario                 VARCHAR(30),
     subcategoria            VARCHAR(30),
     nombre_obligacion       VARCHAR(50),
     descripcion             VARCHAR(100),
-    monto_fijo_mensual      DOUBLE,
+    monto_fijo_mensual      NUMERIC(15,2),
     dia                     INTEGER,
-    vigente                 BIT,
+    vigente                 BIT             DEFAULT 1,
     fecha_inicio            DATE,
     fecha_fin               DATE,
     creado_por              VARCHAR(200),
-    creado_en               TIMESTAMP,
+    creado_en               TIMESTAMP       DEFAULT CURRENT TIMESTAMP,
     modificado_por          VARCHAR(200),
     modificado_en           TIMESTAMP,
     PRIMARY KEY (id_obligacion),
@@ -112,8 +109,8 @@ CREATE TABLE obligacion_fija (
 
 
 CREATE TABLE transaccion (
-    id_transaccion          VARCHAR(30)     NOT NULL,
-    id_presupuesto_detalle  VARCHAR(30),
+    id_transaccion          INTEGER         IDENTITY NOT NULL,
+    id_presupuesto_detalle  INTEGER,
     usuario                 VARCHAR(30),
     presupuesto             VARCHAR(50),
     anio                    INTEGER,
@@ -122,14 +119,14 @@ CREATE TABLE transaccion (
     obligacion              VARCHAR(30),
     tipo_transaccion        VARCHAR(30),
     descripcion             VARCHAR(100),
-    monto                   DOUBLE,
+    monto                   NUMERIC(15,2),
     fecha                   DATE,
     metodo_pago             VARCHAR(50),
     num_factura             INTEGER,
     observaciones           VARCHAR(100),
     fecha_hora_registro     TIMESTAMP,
     creado_por              VARCHAR(200),
-    creado_en               TIMESTAMP,
+    creado_en               TIMESTAMP       DEFAULT CURRENT TIMESTAMP,
     modificado_por          VARCHAR(200),
     modificado_en           TIMESTAMP,
     PRIMARY KEY (id_transaccion),
@@ -138,9 +135,9 @@ CREATE TABLE transaccion (
 
 
 CREATE TABLE obligacion_transaccion (
-    id_transaccion          VARCHAR(30)     NOT NULL,
-    id_obligacion           VARCHAR(30),
-    PRIMARY KEY (id_transaccion, id_obligacion),
+    id_transaccion          INTEGER         NOT NULL,
+    id_obligacion           INTEGER,
+    PRIMARY KEY (id_transaccion),
     FOREIGN KEY (id_obligacion) REFERENCES obligacion_fija (id_obligacion),
     FOREIGN KEY (id_transaccion) REFERENCES transaccion (id_transaccion)
 );
---
+++ database/procedimientos/crud_usuario.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE PROCEDURE sp_insertar_usuario(IN p_nombre VARCHAR,
+IN p_apellido VARCHAR, 
+ p_email, 
+ p_salario_mensual, 
+ p_creado_por,--AUDITORIA 
+)
+BEGIN 
+--LOGICA 
+    INSERT INTO Usuario()
+    VALUES ();
+END; 
